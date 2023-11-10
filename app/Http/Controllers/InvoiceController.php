edcf90f7dc9d6468d1c71d32b73ab314760bdfbe
immaonsombi <immanuel.onsombi@strathmore.edu>
2023-11-10 11:51:36 +0300

    display of invoices

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    //
+    public function get_all_invoice()
+    {
+        $invoice = Invoice::with('customer')->orderBy('id', 'DESC')->get();
+        return response()->json([
+
+            'invoices' => $invoice
+        ], 200);
+    }
 }
--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -1,3 +1,5 @@
 <template>
     <h1>Page Not Found</h1>
+
+    <router-link to="/">Back Home</router-link>
 </template>
